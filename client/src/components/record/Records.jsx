642b22c964c87f0130c494edb59fffd803df17f0
Tsas Tsagaanaa <tsagaanaa0210@gmail.com>
2024-10-01 12:58:50 +0800

    tuesday

--- client/src/components/record/Records.jsx
+++ client/src/components/record/Records.jsx
@@ -7,25 +7,28 @@ export const Records = () => {
     <div className="w-[282px] h-[1080px] border rounded-xl bg-slate-50 mt-6 ml-[120px]">
       <div className="flex flex-col gap-6 mt-6 ml-4">
         <div className="text-2xl font-semibold">Records</div>
-        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        {/* You can open the modal using document.getElementById('ID').showModal() method */}
         <button
-          className="btn w-[250px] h-8 rounded-full bg-[#0166FF] text-[#FFFFFF] text-base font-normal"
-          onClick={() => document.getElementById("my_modal_1").showModal()}
+          className="btn w-[250px] h-8 rounded-full bg-[#0166FF] text-[#FFFFFF]
+        text-base font-normal"
+          onClick={() => document.getElementById("my_modal_3").showModal()}
         >
-          +Add
+          Add Records
         </button>
-        <dialog id="my_modal_1" className="modal w-[792px] h-[512px] ">
+        <dialog id="my_modal_3" className="modal">
           <div className="modal-box w-[792px] h-[512px]">
-            <h3 className="font-bold text-lg">Hello!</h3>
-            <p className="py-4">
-              Press ESC key or click the button below to close
-            </p>
-            <div className="modal-action">
-              <form method="dialog">
-                {/* if there is a button in form, it will close the modal */}
-                <button className="btn">Close</button>
-              </form>
-            </div>
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+            <h3 className="font-semibold text-[#0F172A] text-xl mt-5 ml-6">
+              {" "}
+              Add Records
+            </h3>
+
+            <div className="border-b-2 mt-4 "></div>
           </div>
         </dialog>
 
